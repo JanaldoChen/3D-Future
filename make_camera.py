c500c46cbfb61f680113973fa204e646f2e90e11
LPXTT <peixia.li.dut@gmail.com>
2020-05-30 14:57:42 +0800

    fix bugs

--- make_camera.py
+++ make_camera.py
@@ -2,20 +2,18 @@ import os
 import sys
 import time
 import json
-from progressbar import *
+import numpy as np
 
 from utils.util import write_pickle_file
 from utils.blender_utils import add_camera, get_calibration_matrix_K_from_blender
 
 if __name__ == '__main__':
-	data_root = '../data/Future-3D-Reconstruction'
-	camera_dir = os.path.join(data_root, 'train', 'cameras')
-
-    with open(os.path.join(data_root, 'train', 'train_set.json')) as f:
+    data_root = 'data/Future-3D-Reconstruction'
+    camera_dir = os.path.join(data_root, 'train', 'cameras')
+    with open(os.path.join(data_root, 'train', 'data_info', 'train_set.json')) as f:
         train_set_info = json.load(f)
-    
     for i in range(len(train_set_info)):
-        model_ID = train_set_info[i]
+        model_ID = train_set_info[i]['model']
         trans = train_set_info[i]['pose']['translation']
         rot = train_set_info[i]['pose']['rotation']
         fov = train_set_info[i]['fov']
@@ -27,4 +25,4 @@ if __name__ == '__main__':
         write_pickle_file(os.path.join(camera_dir, model_ID+'.pkl'), {'K': K})
 
 
-    
+
