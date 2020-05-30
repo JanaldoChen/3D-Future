22a4434be8d5145687c73a77e98aed3fb67cce79
LPXTT <peixia.li.dut@gmail.com>
2020-05-30 15:32:21 +0800

    fixed make camera bugs

--- make_camera.py
+++ make_camera.py
@@ -5,24 +5,31 @@ import json
 import numpy as np
 
 from utils.util import write_pickle_file
-from utils.blender_utils import add_camera, get_calibration_matrix_K_from_blender
+from utils.blender_utils import clear_mv, clear_scene, add_camera, get_calibration_matrix_K_from_blender
 
 if __name__ == '__main__':
     data_root = 'data/Future-3D-Reconstruction'
     camera_dir = os.path.join(data_root, 'train', 'cameras')
     with open(os.path.join(data_root, 'train', 'data_info', 'train_set.json')) as f:
         train_set_info = json.load(f)
-    for i in range(len(train_set_info)):
+    total = len(train_set_info)
+    for i in range(total):
         model_ID = train_set_info[i]['model']
         trans = train_set_info[i]['pose']['translation']
         rot = train_set_info[i]['pose']['rotation']
         fov = train_set_info[i]['fov']
-
-        cam = add_camera((0, 0, 0), fov, 'camera')
-        K_blender = get_calibration_matrix_K_from_blender(cam.data)
-        K = np.array(K_blender)
-
-        write_pickle_file(os.path.join(camera_dir, model_ID+'.pkl'), {'K': K})
+        clear_scene()
+        clear_mv()
+        try:
+            cam = add_camera((0, 0, 0), fov, 'camera')
+            K_blender = get_calibration_matrix_K_from_blender(cam.data)
+            K = np.array(K_blender)
+        except:
+            continue
+        cam_path = os.path.join(camera_dir, model_ID+'.pkl')
+        write_pickle_file(cam_path, {'K': K})
+        print('[ {} / {} ] Saved to {}'.format(i, total, cam_path))
+    print('Finished!')
 
 
 
