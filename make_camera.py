28c31a9c59c1061408db4b0caba000451155d7db
JanaldoChen <2032589713@qq.com>
2020-05-30 23:11:51 +0800

    update camera

--- make_camera.py
+++ make_camera.py
@@ -8,6 +8,17 @@ import numpy as np
 from utils.util import write_pickle_file
 from utils.blender_utils import clear_mv, clear_scene, add_camera, get_calibration_matrix_K_from_blender
 
+def init_blender():
+    scene = bpy.context.scene
+    scene.render.resolution_x = 1200
+    scene.render.resolution_y = 1200
+    scene.render.resolution_percentage = 100
+
+    # Delete default cube
+    for obj in bpy.data.objects:
+        obj.select = True
+    bpy.ops.object.delete()
+
 if __name__ == '__main__':
     data_root = 'data/Future-3D-Reconstruction'
     camera_dir = os.path.join(data_root, 'train', 'cameras')
@@ -19,12 +30,13 @@ if __name__ == '__main__':
         trans = np.array(train_set_info[i]['pose']['translation'])
         rot = np.array(train_set_info[i]['pose']['rotation'])
         try:
+            init_blender()
             fov = math.radians(train_set_info[i]['fov'])
-            clear_scene()
-            clear_mv()
             cam = add_camera((0, 0, 0), fov, 'camera')
             K_blender = get_calibration_matrix_K_from_blender(cam.data)
             K = np.array(K_blender)
+            clear_scene()
+            clear_mv()
         except:
             continue
         cam_path = os.path.join(camera_dir, model_ID+'.pkl')
